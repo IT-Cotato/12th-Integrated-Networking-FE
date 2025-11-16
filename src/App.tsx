391e25f14c8e075109241435aff5a158e820fa87
bk-git-hub <bkprivate99@gmail.com>
2025-11-16 20:07:51 +0900

    feat: App.tsx에 SelecteLocationContext 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { useState } from 'react';
+import type { Location } from '@/types/location';
+import { SelectedLocationContext } from './contexts/selected-location-context';
+
 function App() {
-  return <></>;
+  const [selectedLocation, setSelectedLocation] = useState<Location | null>(
+    null,
+  );
+
+  return (
+    <SelectedLocationContext
+      value={{
+        selectedLocation: selectedLocation,
+        selectLocation: setSelectedLocation,
+      }}
+    >
+      <div className="bg-blue"> sdflsj</div>
+    </SelectedLocationContext>
+  );
 }
 
 export default App;
