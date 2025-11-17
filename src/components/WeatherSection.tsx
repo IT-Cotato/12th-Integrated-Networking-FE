f18ff8000c5730b47a75edb5231735bc48f8f487
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-18 02:19:37 +0900

    feat: 홈화면의 weatherSection 레이아웃 추가

--- src/components/WeatherSection.tsx
+++ src/components/WeatherSection.tsx
@@ -1,4 +1,16 @@
-const WeatherSection = () => {
-  return <div>WeatherSection</div>;
+interface WeatherSectionProps {
+  title: string;
+  children: React.ReactNode;
+  gap?: string;
+}
+const WeatherSection = ({ title, children, gap }: WeatherSectionProps) => {
+  return (
+    <div
+      className={`section-shadow p-6 flex flex-col bg-gray-0 border border-gray-10 rounded-2xl ${gap}`}
+    >
+      <div className="text-body-lg text-gray-100">{title}</div>
+      <div>{children}</div>
+    </div>
+  );
 };
 export default WeatherSection;
