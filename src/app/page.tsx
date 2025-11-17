6ecccf1c995b0d8e88013d51d1ed160e912e1762
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-18 02:40:39 +0900

    style: tailwind 속성 정렬

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import WeatherSection from "@/components/WeatherSection";
 
 export default function Home() {
   return (
-    <div className="bg-gray-5 flex flex-col justify-center items-center w-full min-h-screen gap-6">
+    <div className="bg-gray-5 flex min-h-screen w-full flex-col items-center justify-center gap-6">
       <WeatherSection title="4월 26일 롯데월드 날씨 현황" />
       <WeatherSection title="시간별 현황" />
       <WeatherSection title="주간 예보" />
