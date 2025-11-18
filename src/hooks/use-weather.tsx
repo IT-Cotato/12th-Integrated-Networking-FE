d134781618881f55152523d0b9b7a89aab14e6b1
bk-git-hub <bkprivate99@gmail.com>
2025-11-18 16:25:56 +0900

    feat: useWeather 훅 생성 #1

---
+++ src/hooks/use-weather.tsx
@@ -0,0 +1,14 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { fetchWeather } from '@/mocks/fetch-weather';
+import { weatherKeys } from '@/queries/weatherKeys';
+import type { Location } from '@/types/location';
+
+export function useWeather(location: Location | null) {
+  return useSuspenseQuery({
+    queryKey: weatherKeys.byLocation(location),
+    queryFn: async () => {
+      const weatherData = await fetchWeather(location);
+      return weatherData;
+    },
+  });
+}
