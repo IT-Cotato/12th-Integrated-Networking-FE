3f65d4b0a8f86f57618294a0ff4f37d3095f1636
chaewon41 <chaew0m3x@gmail.com>
2025-11-19 23:42:47 +0900

    style: 사이드바 스타일을 피그마 기준으로 수정

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,1 +1,8 @@
-// Sidebar 컴포넌트
+export default function Sidebar() {
+    return (
+      <div className="fixed left-0 top-0 w-[248px] h-[1200px] py-12 px-4 flex flex-col items-start gap-10 rounded-r-[48px] bg-white shadow-[2px_0_4px_rgba(0,0,0,0.10)]">
+        사이드바 스타일 적용
+      </div>
+    );
+  }
+  
