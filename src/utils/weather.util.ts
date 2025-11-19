ec6ed6b2dcdaa271bf6f9121db71fe861c70c9ea
bk-git-hub <bkprivate99@gmail.com>
2025-11-19 15:08:44 +0900

    feat: mapWeatherText 함수 추가 #1

--- src/utils/weather.util.ts
+++ src/utils/weather.util.ts
@@ -10,6 +10,18 @@ const ICON_MAP: { [key: string]: string } = {
   '50': '-wind', // Mist -> wind로 매핑
 };
 
+const TEXT_MAP: { [key: string]: string } = {
+  '01': '맑음',
+  '02': '흐림',
+  '03': '흐림',
+  '04': '흐림',
+  '09': '소나기',
+  '10': '비',
+  '11': '천둥번개',
+  '13': '눈',
+  '50': '안개/박무',
+};
+
 export function convertKelvinToCelsius(
   kelvinTemp: number,
   decimalPlaces: number = 2,
@@ -32,4 +44,15 @@ export function mapWeatherIcon(iconCode: string): string {
 
   weatherIcon = prefix + suffix;
   return weatherIcon;
+}
+
+export function mapWeatherText(iconCode: string): string {
+  const isDay: boolean = iconCode.charAt(2) == 'd' ? true : false;
+  const weatherCode: string = iconCode.substring(0, 2);
+
+  const prefix: string = isDay ? '주간' : '야간';
+  const suffix: string = TEXT_MAP[weatherCode] || '';
+
+  const weatherText = `${prefix} / ${suffix}`;
+  return weatherText;
 }
