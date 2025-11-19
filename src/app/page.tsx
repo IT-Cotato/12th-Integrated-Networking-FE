af09b7191719fd02abb838e19b3a6e1d962f97dd
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-19 15:50:05 +0900

    feat: 장소없을 때 홈화면 ui 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
-import WeatherSection from "@/components/WeatherSection";
+import { formatLocalDate } from "src/utils/getLocalDate";
 
-export default function Home() {
+import Clouds from "@/assets/weather/clouds.svg";
+
+import WeatherSection from "@/components/home/WeatherSection";
+
+const Home = () => {
+  const date = formatLocalDate();
+  // TBD: 사이드바 위치 선택값으로 변경
+  const LOCATION = "롯데월드";
+
   return (
     <div className="bg-gray-5 flex min-h-screen w-full flex-col items-center justify-center gap-6">
-      <WeatherSection title="4월 26일 롯데월드 날씨 현황" />
-      <WeatherSection title="시간별 현황" />
-      <WeatherSection title="주간 예보" />
+      {!LOCATION ? (
+        <>
+          <Clouds className="h-80 w-80" />
+          <div className="text-h2 text-gray-100">
+            아직 선택된 위치가 없습니다!
+          </div>
+        </>
+      ) : (
+        <>
+          <WeatherSection title={`${date} ${LOCATION} 날씨 현황`} />
+          <WeatherSection title="시간별 현황" />
+          <WeatherSection title="주간 예보" />
+        </>
+      )}
     </div>
   );
-}
+};
+export default Home;
