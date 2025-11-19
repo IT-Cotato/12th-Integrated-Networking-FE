3e2fe880f1ed23997a206619c13e48f1a489e9d2
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-19 19:42:19 +0900

    design: 타이틀과 내부 내용 사이 갭 추가

--- src/components/home/WeatherSection.tsx
+++ src/components/home/WeatherSection.tsx
@@ -6,7 +6,7 @@ interface WeatherSectionProps {
 const WeatherSection = ({ title, children, gap }: WeatherSectionProps) => {
   return (
     <div
-      className={`section-shadow bg-gray-0 border-gray-10 flex w-[1080px] flex-col rounded-2xl border p-6 ${gap}`}
+      className={`section-shadow bg-gray-0 border-gray-10 flex w-[1080px] flex-col gap-3 rounded-2xl border p-6 ${gap}`}
     >
       <div className="text-body-lg text-gray-100">{title}</div>
       <div>{children}</div>
