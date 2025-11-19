a3f87651f3b739ee38454bd3c96e3e028ce8c119
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-20 00:46:47 +0900

    feat: 요일 반환하는 유틸 추가

--- src/utils/getLocalDate.ts
+++ src/utils/getLocalDate.ts
@@ -1,7 +1,37 @@
-export const formatLocalDate = () => {
-  const currentDate = new Date();
-  const month = currentDate.getMonth() + 1;
-  const day = currentDate.getDate();
+const getTodayParts = () => {
+  const now = new Date();
+  return {
+    month: now.getMonth() + 1,
+    day: now.getDate(),
+  };
+};
 
+// "11월 19일" 형식
+export const formatLocalDate = () => {
+  const { month, day } = getTodayParts();
   return `${month}월 ${day}일`;
+};
+
+// "11.19" 형식
+export const formatLocalDateWithDot = () => {
+  const { month, day } = getTodayParts();
+  return `${month}.${day}`;
+};
+
+export const getKoreanDayLabel = (dateString: string) => {
+  const target = new Date(dateString);
+  const today = new Date();
+
+  // 날짜 비교 (연/월/일만 비교)
+  const isToday =
+    target.getFullYear() === today.getFullYear() &&
+    target.getMonth() === today.getMonth() &&
+    target.getDate() === today.getDate();
+
+  if (isToday) return "오늘";
+
+  const day = target.getDay(); // 0~6
+  const DAY_LABEL = ["일", "월", "화", "수", "목", "금", "토"];
+
+  return DAY_LABEL[day];
 };
