c98a3b83d017a536cb71568737b953c59ab3ea78
bk-git-hub <bkprivate99@gmail.com>
2025-11-19 18:59:58 +0900

    chore: fetch weather 함수 로딩 시간 수정

--- src/mocks/fetch-weather.ts
+++ src/mocks/fetch-weather.ts
@@ -5,14 +5,31 @@ import type { Location } from '@/types/location';
 export const fetchWeather = (
   location: Location | null,
 ): Promise<OneCallResponse> => {
-  console.log(`[Mock API] ${location?.name}의 One Call 날씨 정보 요청 중...`);
+  if (!location) {
+    console.error('[Mock API] Error: Location is null. Cannot fetch weather.');
+    return Promise.reject(new Error('Location is required to fetch weather.'));
+  }
+
+  const lat = location.y;
+  const lon = location.x;
+
+  console.log(
+    `[Mock API] ${location.name} 날씨 정보 요청. (lat: ${lat}, lon: ${lon} 전송)`,
+  );
+
+  /*
+  const response = await axios.get('/api/weather-server/weather', { 
+    params: { lat, lon } 
+  });
+  return response.data;
+  */
 
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(mockRes as OneCallResponse);
-    }, 500);
+    }, 1000);
   });
 };
 
-// 5. TODO: (별도 구현 필요) 미세먼지 API 호출 함수
+// TODO: (별도 구현 필요) 미세먼지 API 호출 함수
 // export const fetchAirPollution = (location: Location | null): Promise<AirPollutionResponse> => { ... }
