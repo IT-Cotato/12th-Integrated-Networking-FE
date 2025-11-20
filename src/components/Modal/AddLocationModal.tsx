f7744940edbaf526ba9ff4d45327e0cce38831ca
chaewon41 <chaew0m3x@gmail.com>
2025-11-20 11:40:29 +0900

    feat: 모달 상단 추가

--- src/components/Modal/AddLocationModal.tsx
+++ src/components/Modal/AddLocationModal.tsx
@@ -12,10 +12,24 @@ export default function AddLocationModal({ isOpen, onClose }: Props) {
       onClick={onClose}
     >
       <div 
-        className="bg-white rounded-2xl w-[624px] h-[641px] pt-9 pr-[72px] pb-9 pl-[72px] flex flex-col gap-12 shadow-[4px_4px_4px_3px_#00000040]"
+        className="bg-white rounded-2xl w-[624px] h-[641px] pt-9 pr-[72px] pb-9 pl-[72px] shadow-[4px_4px_4px_3px_#00000040]"
         onClick={(e) => e.stopPropagation()}
       >
-        {/* 빈 모달 화면 */}
+        {/* 모달 헤더와 검색 영역을 감싸는 컨테이너 */}
+        <div className="flex flex-col items-start gap-12 h-full">
+          {/* 모달 헤더 */}
+          <div className="flex items-center gap-4">
+            <img src="/Day Clouds.svg" alt="날씨" className="w-20 h-20" />
+            <span 
+              className="text-[#292E2E] font-bold text-[32px] leading-[100%] tracking-normal"
+              style={{ fontFamily: 'Pretendard, sans-serif' }}
+            >
+              날씨 위치 추가
+            </span>
+          </div>
+
+          {/* 위치 검색 영역 (추가 예정) */}
+        </div>
       </div>
     </div>
   );
