40765c8f8677f05a115002db882ba83581e1fe1d
metjyc <metjyc@naver.com>
2025-11-21 09:28:11 +0900

    feat: sidebar Props 정의 및 타입 지정

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,11 +2,26 @@
 
 import MapPin from "@/assets/icons/map-pin.svg";
 
-type SidebarProps = object;
+import { Location } from "@/types/location";
 
-export default function Sidebar() {
+type SidebarProps = {
+  locations: Location[];
+  selectedId: string | null;
+  onSelect: (id: string) => void;
+  onClickAdd: () => void;
+  onClickDelete: (id: string) => void;
+};
+
+export default function Sidebar({
+  locations,
+  selectedId,
+  onSelect,
+  onClickAdd,
+  onClickDelete,
+}: SidebarProps) {
   return (
     <aside className="h-screen w-64 border-r border-gray-200 bg-white px-6 py-8">
+      {/* 상단 헤더 */}
       <div className="mb-8">
         <div className="mb-2 flex items-center gap-2 text-sm font-semibold">
           <span className="text-red-500">
