c0c5aeb0daced7d2b2989aa4b804dbc287aa7b7c
bk-git-hub <bkprivate99@gmail.com>
2025-11-23 23:01:48 +0900

    feat: 주간예보의 일간정보를 담는 컴포넌트 추가

---
+++ src/components/main/daily-weather.tsx
@@ -0,0 +1,46 @@
+import type { WeeklyWeather } from '@/types/weather.types';
+import { getDayLabel, mapWeatherIcon } from '@/utils/weather.util';
+import PoPText from './pop-text';
+
+interface DailyWeatherProps {
+  data: WeeklyWeather;
+}
+
+export default function DailyWeather({ data }: DailyWeatherProps) {
+  const morningIcon = mapWeatherIcon(data.morningIcon);
+  const eveningIcon = mapWeatherIcon(data.eveningIcon);
+  const minTemp = Number(data.min.toFixed(1));
+  const maxTemp = Number(data.max.toFixed(1));
+  const dayLabel = getDayLabel(data.date);
+
+  return (
+    <div className="flex w-fit flex-col gap-2 rounded-2xl border-2 p-2">
+      <div className="flex gap-4">
+        <div className="flex flex-col items-center gap-3">
+          <div className="flex flex-col items-center">
+            <span>오전</span>{' '}
+            <img src={`${morningIcon}`} width={60} height={60} />
+          </div>
+
+          <PoPText PoP={data.morningPop.toString()} />
+          <span className="font-bold">최저</span>
+          <span className="text-blue font-bold">{minTemp}º</span>
+        </div>
+
+        <div className="flex flex-col items-center gap-3">
+          <div className="flex flex-col items-center">
+            <span>오후</span>
+            <img src={`${eveningIcon}`} width={60} height={60} />
+          </div>
+          <PoPText PoP={data.morningPop.toString()} />
+          <span className="font-bold">최고</span>
+          <span className="text-red font-bold">{maxTemp}º</span>
+        </div>
+      </div>
+      <div className="flex flex-col items-center font-medium">
+        <span>{data.date}</span>
+        <span>{dayLabel}</span>
+      </div>
+    </div>
+  );
+}
